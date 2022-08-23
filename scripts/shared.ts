b6efebfe40838c94b52fe168b1869cee397b9d5e
GalvinGao <me@galvingao.com>
2022-08-23 04:58:16 -0400

    fix: remove char_512_aprot

--- scripts/shared.ts
+++ scripts/shared.ts
@@ -1,4 +1,4 @@
-import { uniq } from 'lodash-es'
+import { uniqBy } from 'lodash-es'
 import fetch from 'node-fetch'
 import pinyin from 'pinyin'
 
@@ -23,12 +23,16 @@ function transformOperatorName(name: string) {
 const CHARACTER_TABLE_JSON_URL =
   'https://raw.githubusercontent.com/Kengxxiao/ArknightsGameData/master/zh_CN/gamedata/excel/character_table.json'
 
+const CHARACTER_BLOCKLIST = [
+  'char_512_aprot', // 暮落(集成战略)：It's just not gonna be there.
+]
+
 export async function getOperatorNames() {
   const resp = (await fetch(CHARACTER_TABLE_JSON_URL).then((res) =>
     res.json(),
   )) as any
   const ids = Object.keys(resp)
-  return uniq(
+  const result = uniqBy(
     ids.flatMap((el) => {
       const op = resp[el]
       if (['TOKEN', 'TRAP'].includes(op.profession)) return []
@@ -39,7 +43,9 @@ export async function getOperatorNames() {
         },
       ]
     }),
+    (el) => el.name,
   ).sort((a, b) => {
     return pinyin.compare(a.name, b.name)
   })
+  return result.filter((el) => !CHARACTER_BLOCKLIST.includes(el.id))
 }
