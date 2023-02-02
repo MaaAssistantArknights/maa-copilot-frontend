28af7c7ab5742dc4b3e12df18897745d6d5b5c6a
FutabaRio <futabario52@gmail.com>
2023-02-02 18:21:50 +0800

    fix: 在 unmount 时移除监听

--- src/components/BackToTop.tsx
+++ src/components/BackToTop.tsx
@@ -5,13 +5,17 @@ import { useEffect, useState } from 'react'
 export const BackToTop = () => {
   const [showButton, setShowButton] = useState(false)
   useEffect(() => {
-    document.addEventListener('scroll', () => {
+    const handleScroll = () => {
       if (window.pageYOffset > 100) {
         setShowButton(true)
       } else {
         setShowButton(false)
       }
-    })
+    }
+    document.addEventListener('scroll', handleScroll)
+    return () => {
+      document.removeEventListener('scroll', handleScroll)
+    }
   }, [])
   const handleClickButton = () => {
     window.scrollTo({ top: 0, behavior: 'smooth' })
