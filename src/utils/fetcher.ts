5d2dd6b464735418225e78e1dce4525969de98e6
Guan <821143943@qq.com>
2023-02-09 13:57:10 +0800

    feat: friendly error message for 401

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -28,50 +28,60 @@ export const request = <T extends Response<unknown>>(
 ): Promise<T> =>
   Promise.resolve(
     init?.noToken ? undefined : FETCHER_CONFIG.apiToken?.().catch(console.warn),
-  )
-    .then((apiToken) =>
-      fetch(
-        baseURL + input,
-        merge(
-          init,
-          apiToken && {
-            headers: {
-              Authorization: `Bearer ${apiToken}`,
-            },
+  ).then((apiToken) =>
+    fetch(
+      baseURL + input,
+      merge(
+        init,
+        apiToken && {
+          headers: {
+            Authorization: `Bearer ${apiToken}`,
           },
-        ),
+        },
       ),
     )
-    .then(async (res) => {
-      return {
-        response: res,
-        data: camelcaseKeys(await res.json(), { deep: true }) as T,
-      }
-    })
-    .catch((err) => {
-      console.error('Fetcher: got error', err)
-      return Promise.reject(new Error('网络错误，请检查网络连接并稍后重试'))
-    })
-    .then((res) => {
-      if (
-        (res.data.statusCode &&
-          (res.data.statusCode < 200 || res.data.statusCode >= 300)) ||
-        res.response.status < 200 ||
-        res.response.status >= 300
-      ) {
-        console.error('Fetcher: got error response', res)
-        return Promise.reject(
-          new Error(
-            res.data?.message ||
+      .then(async (res) => {
+        return {
+          response: res,
+          data: camelcaseKeys(await res.json(), { deep: true }) as T,
+        }
+      })
+
+      .catch((err) => {
+        console.error('Fetcher: got error', err)
+        return Promise.reject(new Error('网络错误，请检查网络连接并稍后重试'))
+      })
+      .then((res) => {
+        if (
+          (res.data.statusCode &&
+            (res.data.statusCode < 200 || res.data.statusCode >= 300)) ||
+          res.response.status < 200 ||
+          res.response.status >= 300
+        ) {
+          console.error('Fetcher: got error response', res)
+
+          let message: string
+
+          if (res.response.status === 401 || res.data.statusCode === 401) {
+            if (apiToken) {
+              message = '登录已失效，请重新登录'
+            } else {
+              message = '未登录，请先登录'
+            }
+          } else {
+            message =
+              res.data?.message ||
               JSON.stringify(res.data) ||
               `Unknown error (${
                 res.data.statusCode || res.response.status || 'unknown'
-              })`,
-          ),
-        )
-      }
-      return res.data
-    })
+              })`
+          }
+
+          throw new Error(message)
+        }
+        return res.data
+      }),
+  )
 
 export type JsonRequestInit = ExtendedRequestInit & {
   json?: any
