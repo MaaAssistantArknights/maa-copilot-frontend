9aaef3d1827e3164f884506df30e488e8c748f58
Guan <821143943@qq.com>
2024-03-20 19:17:27 +0800

    refactor: read neoLayout flag from atom instead of props

--- src/components/OperationSetList.tsx
+++ src/components/OperationSetList.tsx
@@ -1,74 +1,78 @@
 import { Button, NonIdealState } from '@blueprintjs/core'
 
 import { UseOperationSetsParams, useOperationSets } from 'apis/operation-set'
+import { useAtomValue } from 'jotai'
 import { ComponentType, ReactNode } from 'react'
+
+import { neoLayoutAtom } from 'store/pref'
 
 import { NeoOperationSetCard, OperationSetCard } from './OperationSetCard'
 import { withSuspensable } from './Suspensable'
 
-export const OperationSetList: ComponentType<
-  UseOperationSetsParams & { neoLayout?: boolean }
-> = withSuspensable(
-  (props) => {
-    const { operationSets, setSize, isValidating, isReachingEnd } =
-      useOperationSets({ ...props, suspense: true })
+export const OperationSetList: ComponentType<UseOperationSetsParams> =
+  withSuspensable(
+    (props) => {
+      const neoLayout = useAtomValue(neoLayoutAtom)
 
-    // make TS happy: we got Suspense out there
-    if (!operationSets) throw new Error('unreachable')
+      const { operationSets, setSize, isValidating, isReachingEnd } =
+        useOperationSets({ ...props, suspense: true })
 
-    const { neoLayout = true } = props
+      // make TS happy: we got Suspense out there
+      if (!operationSets) throw new Error('unreachable')
 
-    const items: ReactNode = neoLayout ? (
-      <div
-        className="grid gap-4"
-        style={{ gridTemplateColumns: 'repeat(auto-fill, minmax(20rem, 1fr)' }}
-      >
-        {operationSets?.map((operationSet) => (
-          <NeoOperationSetCard
-            operationSet={operationSet}
-            key={operationSet.id}
-          />
-        ))}
-      </div>
-    ) : (
-      operationSets.map((operationSet) => (
-        <OperationSetCard operationSet={operationSet} key={operationSet.id} />
-      ))
-    )
+      const items: ReactNode = neoLayout ? (
+        <div
+          className="grid gap-4"
+          style={{
+            gridTemplateColumns: 'repeat(auto-fill, minmax(20rem, 1fr)',
+          }}
+        >
+          {operationSets?.map((operationSet) => (
+            <NeoOperationSetCard
+              operationSet={operationSet}
+              key={operationSet.id}
+            />
+          ))}
+        </div>
+      ) : (
+        operationSets.map((operationSet) => (
+          <OperationSetCard operationSet={operationSet} key={operationSet.id} />
+        ))
+      )
 
-    return (
-      <>
-        {items}
+      return (
+        <>
+          {items}
 
-        {isReachingEnd && operationSets.length === 0 && (
-          <NonIdealState
-            icon="slash"
-            title="没有找到任何作业集"
-            description="(つД｀)･ﾟ･"
-          />
-        )}
+          {isReachingEnd && operationSets.length === 0 && (
+            <NonIdealState
+              icon="slash"
+              title="没有找到任何作业集"
+              description="(つД｀)･ﾟ･"
+            />
+          )}
 
-        {isReachingEnd && operationSets.length !== 0 && (
-          <div className="mt-8 w-full tracking-wider text-center select-none text-slate-500">
-            已经到底了哦 (ﾟ▽ﾟ)/
-          </div>
-        )}
+          {isReachingEnd && operationSets.length !== 0 && (
+            <div className="mt-8 w-full tracking-wider text-center select-none text-slate-500">
+              已经到底了哦 (ﾟ▽ﾟ)/
+            </div>
+          )}
 
-        {!isReachingEnd && (
-          <Button
-            loading={isValidating}
-            text="加载更多"
-            icon="more"
-            className="mt-2"
-            large
-            fill
-            onClick={() => setSize((size) => size + 1)}
-          />
-        )}
-      </>
-    )
-  },
-  {
-    retryOnChange: ['keyword'],
-  },
-)
+          {!isReachingEnd && (
+            <Button
+              loading={isValidating}
+              text="加载更多"
+              icon="more"
+              className="mt-2"
+              large
+              fill
+              onClick={() => setSize((size) => size + 1)}
+            />
+          )}
+        </>
+      )
+    },
+    {
+      retryOnChange: ['keyword'],
+    },
+  )
