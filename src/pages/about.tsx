5e870b2c13f74a1cc636db7eb2e51af1016c1bc5
Guan <821143943@qq.com>
2024-03-29 20:28:15 +0800

    fix: rainbow text in dark mode

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -10,9 +10,9 @@ export const AboutPage = () => {
     <div className="max-w-[48rem] mx-auto">
       <div className="mt-8 flex flex-col items-center">
         <div className="">
-          <div className="bg-rainbow relative text-transparent !bg-clip-text font-bold italic text-7xl leading-[1.2]">
-            <div className="ml-48 text-6xl">作业站解君愁</div>
-            <div className="mr-48">点击收藏不迷路</div>
+          <div className="bg-rainbow relative !text-transparent !bg-clip-text font-bold italic text-7xl leading-[1.2]">
+            <div className="ml-48 text-6xl !text-inherit">作业站解君愁</div>
+            <div className="mr-48 !text-inherit">点击收藏不迷路</div>
           </div>
           <div className="bg-rainbow !bg-clip-content h-1.5 pr-48" />
         </div>
