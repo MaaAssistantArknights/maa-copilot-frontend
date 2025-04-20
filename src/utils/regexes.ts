b85f482fc47ac3b9e49005c27dff1f4875571665
dragove <dragove@qq.com>
2025-04-20 17:56:02 +0800

    feat: add username trim validation

--- src/utils/regexes.ts
+++ src/utils/regexes.ts
@@ -2,3 +2,5 @@
 // let the server do the validation further and, if an email cannot be sent,
 // a sufficient error message is far better than tied-to-RFC validation on client side.
 export const REGEX_EMAIL = /^\S+@\S+\.\S+$/
+// 开头和结尾不能为空格，但是中间允许有空格
+export const REGEX_USERNAME = /^\S.{2,22}\S$/
