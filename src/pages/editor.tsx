78f9ee4e60afd16a340c09a4fca691e1533dfe45
Guan <821143943@qq.com>
2025-04-30 20:50:09 +0800

    fix(editor2): unable to submit

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -2,7 +2,7 @@ import { Provider } from 'jotai'
 import { useAtomDevtools } from 'jotai-devtools'
 import { useAtomCallback } from 'jotai/utils'
 import { CopilotInfoStatusEnum } from 'maa-copilot-client'
-import { useCallback, useMemo } from 'react'
+import { FC, useCallback, useMemo } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 
 import { withGlobalErrorBoundary } from 'components/GlobalErrorBoundary'
@@ -13,7 +13,6 @@ import {
   updateOperation,
   useOperation,
 } from '../apis/operation'
-import { withSuspensable } from '../components/Suspensable'
 import { AppToaster } from '../components/Toaster'
 import {
   defaultEditorState,
@@ -31,118 +30,119 @@ import { formatError } from '../utils/error'
 import { AtomsHydrator } from '../utils/react'
 import { wrapErrorMessage } from '../utils/wrapErrorMessage'
 
-export const EditorPage = withGlobalErrorBoundary(
-  withSuspensable(() => {
-    const params = useParams()
-    const navigate = useNavigate()
-    const id = params.id ? +params.id : undefined
-    const isNew = !id
-    const apiOperation = useOperation({
-      id,
-      suspense: true,
-      revalidateOnFocus: false,
-      revalidateIfStale: false,
-      revalidateOnReconnect: false,
-    }).data
+const _EditorPage: FC = () => {
+  const params = useParams()
+  const navigate = useNavigate()
+  const id = params.id ? +params.id : undefined
+  const isNew = !id
+  const apiOperation = useOperation({
+    id,
+    suspense: true,
+    revalidateOnFocus: false,
+    revalidateIfStale: false,
+    revalidateOnReconnect: false,
+  }).data
 
-    if (process.env.NODE_ENV === 'development') {
-      useAtomDevtools(historyAtom, { name: 'editorStateAtom' })
-    }
+  if (process.env.NODE_ENV === 'development') {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useAtomDevtools(historyAtom, { name: 'editorStateAtom' })
+  }
 
-    const initialEditorAtomValue = useMemo(
-      () =>
+  const initialEditorAtomValue = useMemo(
+    () =>
+      [
         [
-          [
-            editorAtoms.editor,
-            apiOperation
-              ? {
-                  operation: toEditorOperation(
-                    operationLooseSchema.parse(
-                      JSON.parse(apiOperation.content),
-                    ),
-                  ),
-                  metadata: {
-                    visibility:
-                      apiOperation.status === CopilotInfoStatusEnum.Public
-                        ? 'public'
-                        : 'private',
-                  },
-                }
-              : defaultEditorState,
-          ],
-        ] as const,
-      [apiOperation],
-    )
-
-    const handleSubmit = useAtomCallback(
-      useCallback(
-        async (get, set) => {
-          const result = set(editorValidationAtom)
-          if (!result.success) {
-            set(editorErrorsVisibleAtom, true)
-            AppToaster.show({
-              message: '作业内容存在错误，请检查',
-              intent: 'danger',
-            })
-            return false
-          }
-          const operation = result.data
-          const status =
-            get(editorAtoms.metadata).visibility === 'public'
-              ? CopilotInfoStatusEnum.Public
-              : CopilotInfoStatusEnum.Private
-
-          const upload = async () => {
-            if (id) {
-              await updateOperation({
-                id,
-                content: JSON.stringify(operation),
-                status,
-              })
-              AppToaster.show({
-                message: '作业更新成功',
-                intent: 'success',
-              })
-              navigate(`/?op=${id}`)
-            } else {
-              const newId = await createOperation({
-                content: JSON.stringify(operation),
-                status,
-              })
-              AppToaster.show({
-                message: '作业创建成功',
-                intent: 'success',
-              })
-              if (newId) {
-                navigate(`/?op=${newId}`)
-              } else {
-                navigate('/')
+          editorAtoms.editor,
+          apiOperation
+            ? {
+                operation: toEditorOperation(
+                  operationLooseSchema.parse(JSON.parse(apiOperation.content)),
+                ),
+                metadata: {
+                  visibility:
+                    apiOperation.status === CopilotInfoStatusEnum.Public
+                      ? 'public'
+                      : 'private',
+                },
               }
+            : defaultEditorState,
+        ],
+      ] as const,
+    [apiOperation],
+  )
+
+  const handleSubmit = useAtomCallback(
+    useCallback(
+      async (get, set) => {
+        const result = set(editorValidationAtom)
+        if (!result.success) {
+          set(editorErrorsVisibleAtom, true)
+          AppToaster.show({
+            message: '作业内容存在错误，请检查',
+            intent: 'danger',
+          })
+          return false
+        }
+        const operation = result.data
+        const status =
+          get(editorAtoms.metadata).visibility === 'public'
+            ? CopilotInfoStatusEnum.Public
+            : CopilotInfoStatusEnum.Private
+
+        const upload = async () => {
+          if (id) {
+            await updateOperation({
+              id,
+              content: JSON.stringify(operation),
+              status,
+            })
+            AppToaster.show({
+              message: '作业更新成功',
+              intent: 'success',
+            })
+            navigate(`/?op=${id}`)
+          } else {
+            const newId = await createOperation({
+              content: JSON.stringify(operation),
+              status,
+            })
+            AppToaster.show({
+              message: '作业创建成功',
+              intent: 'success',
+            })
+            if (newId) {
+              navigate(`/?op=${newId}`)
+            } else {
+              navigate('/')
             }
           }
+        }
 
-          await wrapErrorMessage(
-            (e) => '上传失败: ' + formatError(e),
-            upload(),
-          ).catch(console.warn)
+        await wrapErrorMessage(
+          (e) => '上传失败: ' + formatError(e),
+          upload(),
+        ).catch(console.warn)
 
-          return true
-        },
-        [id, navigate],
-      ),
-    )
+        return true
+      },
+      [id, navigate],
+    ),
+  )
 
-    return (
-      <Provider>
-        <AtomsHydrator atomValues={initialEditorAtomValue}>
-          <OperationEditor
-            title={isNew ? '创建作业' : '修改作业 - ' + toShortCode({ id })}
-            submitAction={isNew ? '发布作业' : '更新作业'}
-            onSubmit={handleSubmit}
-          />
-        </AtomsHydrator>
-      </Provider>
-    )
-  }),
-)
+  return (
+    <AtomsHydrator atomValues={initialEditorAtomValue}>
+      <OperationEditor
+        title={isNew ? '创建作业' : '修改作业 - ' + toShortCode({ id })}
+        submitAction={isNew ? '发布作业' : '更新作业'}
+        onSubmit={handleSubmit}
+      />
+    </AtomsHydrator>
+  )
+}
+
+export const EditorPage = withGlobalErrorBoundary(() => (
+  <Provider>
+    <_EditorPage />
+  </Provider>
+))
 EditorPage.displayName = 'EditorPage'
