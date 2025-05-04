3c25b7a4b54eb64211cc4e0c7ca0f7d48a86407e
MistEO <MistEO@users.noreply.github.com>
2025-05-04 18:03:57 +0000

    deploy: 093e1dd3ee2b4239105c3b31c7e731874429ea94

---
+++ assets/about-4845366c.js
@@ -0,0 +1,95 @@
+import{a3 as n,a5 as s,bT as t,bU as h}from"./index-b70051f6.js";const a=`## 2025-05-02
+
+- 添加用户名重复校验 [@dragove](https://github.com/dragove)
+
+## 2025-04-19
+
+- 首页作业多选功能 [@guansss](https://github.com/guansss)
+- 作业集内自动排序功能 [@guansss](https://github.com/guansss)
+- 优化了资源缓存 [@martinwang2002](https://github.com/martinwang2002)
+
+## 2025-04-10
+
+- 修复了搜索时按关卡过滤不够精准的问题 [@Aliothmoon](https://github.com/Aliothmoon)
+- 修改作业时会在新标签页打开 [@guansss](https://github.com/guansss)
+- 对搜索面板进行了正面优化（确信） [@guansss](https://github.com/guansss)
+- 广告更新
+
+## 2025-04-04
+
+- 优化了首页列表的性能 [@Aliothmoon](https://github.com/Aliothmoon)
+- 添加了私有作业功能 [@guansss](https://github.com/guansss) [@dragove](https://github.com/dragove)
+- 添加了关闭评论区功能 [@guansss](https://github.com/guansss) [@dragove](https://github.com/dragove)
+- 添加了按用户筛选功能 [@guansss](https://github.com/guansss) [@Pleasurecruise](https://github.com/Pleasurecruise) [@dragove](https://github.com/dragove)
+- 重新设计了主页的搜索面板 [@guansss](https://github.com/guansss)
+- 搜索功能的全文索引优化 [@Aliothmoon](https://github.com/Aliothmoon) [@Handiwork](https://github.com/Handiwork)
+- 修复了按干员筛选时和异格冲突的问题 [@LuoRenMu](https://github.com/LuoRenMu)
+- 优化了敏感词检测 [@dragove](https://github.com/dragove) [@Daydreamer114](https://github.com/Daydreamer114)
+
+从现在起，后端的更新和贡献者也会一并列出 (～￣▽￣)～
+
+## 2025-03-15
+
+- 优化了暗色模式界面显示 [@zhangchi0104](https://github.com/zhangchi0104)
+
+## 2025-03-01
+
+- 优化了移动端界面 [@guansss](https://github.com/guansss)
+- 加入广告 [@guansss](https://github.com/guansss)
+
+## 2025-02-22
+
+- 添加了关卡筛选器的联想输入功能 [@guansss](https://github.com/guansss)
+- 添加了用户个人页面 [@guansss](https://github.com/guansss)
+- 修复了在作业详情里无法正常使用鼠标回退键的问题 [@guansss](https://github.com/guansss)
+- 优化了作业列表和作业详情界面 [@guansss](https://github.com/guansss)
+
+## 2025-02-17
+
+- 修复了编辑作业时无法修改干员名称的问题 [@Gemini2035](https://github.com/Gemini2035)
+- 修复了编辑作业时无法修改动作类型的问题 [@Gemini2035](https://github.com/Gemini2035)
+- 添加了是否保存干员过滤器的选项 [@guansss](https://github.com/guansss)
+- 使用自定义关卡时支持设置难度 [@guansss](https://github.com/guansss)
+- 编辑作业时提醒缺失的干员 [@guansss](https://github.com/guansss)
+
+## 2025-02-06
+
+- 支持作业集内排序 [@hmydgz](https://github.com/hmydgz)
+- 减少首页显示的数据量 [@guansss](https://github.com/guansss)
+
+## 2024-09-06
+
+- 修复了作业卡片高度不足的问题 [@Handiwork](https://github.com/Handiwork)
+- 优化了小屏幕上导航栏的显示 [@martinwang2002](https://github.com/martinwang2002)
+- 优化了干员快捷编辑器 [@Gemini2035](https://github.com/Gemini2035)
+- 优化了首页侧边栏显示 [@fundgao](https://github.com/fundgao)
+- 支持作者评论字数上限为500 [@dragove](https://github.com/dragove)
+
+## 2024-06-19
+
+- 修复了不能通过神秘代码搜索作业集的问题 [@guansss](https://github.com/guansss)
+- 增加了对未激活账号的提示 [@guansss](https://github.com/guansss)
+- 在作业编辑器中添加了额外的提交按钮 [@aojunhao123](https://github.com/aojunhao123)
+- 添加了 Google Analytics [@AlisaAkiron](https://github.com/AlisaAkiron)
+- 改善了 SEO [@HauKuen](https://github.com/HauKuen)
+- 修复暗黑模式下干员筛选标签背景色 [@aojunhao123](https://github.com/aojunhao123)
+- 搜索时填写的包含/排除干员将会自动保存 [@zhangchi0104](https://github.com/zhangchi0104)
+- 修复了切换搜索模式时输入框显示不正常的问题 [@guansss](https://github.com/guansss)
+- 修复了暗黑模式的一些显示问题 [@ssoda01](https://github.com/ssoda01)
+
+## 2024-04-03
+
+- 修复了因部分作业缺少动作列表而渲染失败的问题
+- 修复了点击作业（集）卡片上的按钮时会跳转到详情页的问题
+- 修复了作业集编辑器的保存按钮失效的问题
+- 优化了一些界面布局
+
+## 2024-04-01
+
+- 添加了作业集功能
+- 添加了更新日志功能
+- 添加了对浏览器“返回”和“前进”按钮的支持
+- 调整了快捷干员选择器中的文字提示样式，让其能适应夜间模式
+- 修复了无法搜索神秘代码的问题
+- 修复了无法在编辑器中导入神秘代码的问题
+`,i=()=>n("div",{className:"max-w-screen-md mx-auto",children:[s("div",{className:"mt-8 flex flex-col items-center",children:n("div",{className:"text-[2rem] md:text-[3.75rem]",children:[n("div",{className:"bg-rainbow relative !text-transparent !bg-clip-text font-bold italic leading-[1.2]",children:[s("div",{className:"ml-[3.2em] mr-[0.2em] !text-inherit",children:"作业站解君愁"}),s("div",{className:"text-[1.2em] !text-inherit",children:"点个收藏不迷路"})]}),s("div",{className:"bg-rainbow !bg-clip-content h-[0.1em] pr-[1em]"})]})}),n("div",{className:"mt-12 p-4",children:[s(t,{children:"更新日志"}),s(h,{children:a})]})]});export{i as AboutPage};
